5114f141dc52b2866c1f81c59ab59528b82398f4
Тимур Алиев <timuraliev@MacBook-Pro-Timur.local>
2025-09-22 19:40:46 +0700

    9

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import About, Rules
 
 app_name = 'pages'
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,26 +2,24 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class AboutView(TemplateView):
+class About(TemplateView):
     template_name = 'pages/about.html'
 
 
-class RulesView(TemplateView):
+class Rules(TemplateView):
     template_name = 'pages/rules.html'
 
 
 def page_not_found(request, exception):
+    """Кастомная 404."""
     return render(request, 'pages/404.html', status=404)
 
 
 def server_error(request):
+    """Кастомная 500."""
     return render(request, 'pages/500.html', status=500)
 
 
 def csrf_failure(request, reason=''):
-    return render(
-        request,
-        'pages/403_csrf.html',
-        {'reason': reason},
-        status=403,
-    )
+    """Кастомная 403 CSRF."""
+    return render(request, 'pages/403_csrf.html', status=403)
