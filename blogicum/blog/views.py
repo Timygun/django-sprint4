40fc3a8f5dd2fdb1137e6c77840b60a90de620d1
Тимур Алиев <timuraliev@MacBook-Pro-Timur.local>
2025-09-23 07:34:24 +0700

    15

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.views.generic import (
     CreateView,
     DeleteView,
